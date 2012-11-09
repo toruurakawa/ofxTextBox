70f74f06119cf9d256b60f7f383c704295aa1a16
Toru Urakawa <toru.urkw@gmail.com>
2012-11-09 18:02:54 +0900

    Update: resizing font size

--- src/ofxTextBox.h
+++ src/ofxTextBox.h
@@ -10,21 +10,20 @@
 #include "ofMain.h"
 #include "ofxTrueTypeFontUC.h"
 
-
 class ofxTextBox
 {
 public:
     ofxTextBox()
     {
         _text  = L"";
-        _font  = nil;
         _bShowFrame = true;
+        _bAutoResizeFontSize = false;
+        _bOmitted = false;
     }
     
     virtual ~ofxTextBox()
     {
         _text  = L"";
-        _font  = nil;
     }
     
     void setText(string text)
@@ -39,9 +38,11 @@ public:
         refresh();
     }
     
-    void setFont(ofxTrueTypeFontUC *font)
+    void loadFont(string name, int size)
     {
-        _font = font;
+        _fontName = name;
+        _fontSize = size;
+        _font.loadFont(_fontName, _fontSize);
         refresh();
     }
     
@@ -49,29 +50,30 @@ public:
         _bShowFrame = b;
     }
     
+    void setAutoResizeFontSize(bool b){
+        _bAutoResizeFontSize = b;
+        refresh();
+    }
+    
     void refresh()
     {
-        if (_text.c_str() != nil && _rect.width > 0 && _rect.height > 0 && _font != nil){
-            _drawingText = _text;
-            _drawingText.insert(0, L"\n");
-            ofRectangle r = _font->getStringBoundingBox(_text, _rect.x, _rect.y);
-            if (r.width > _rect.width || r.height > _rect.height) {
-                int i = 0;
-                wstring s_left = _text.substr(1);
-                while (_font->getStringBoundingBox(s_left, 0, 0).width > _rect.width) {
-                    wstring tmp_s = L"";
-                    int j = 0;
-                    while (_font->getStringBoundingBox(tmp_s,0,0).width < _rect.width) {
-                        tmp_s = _drawingText.substr(i,j);
-                        j++;
-                    }
-                    i += j-1;
-                    _drawingText.insert(i-1, L"\n");
-                    s_left = _drawingText.substr(i);
-                }
-                i = 0;
-                while (_font->getStringBoundingBox(_drawingText,0,0).height > _rect.height){
-                    _drawingText = _drawingText.substr(0, _drawingText.length()-1);
+        if (_text.c_str() != nil && _rect.width > 0 && _rect.height > 0 ){
+            reconstructText();
+            while (_font.getStringBoundingBox(_drawingText,0,0).height > _rect.height){
+                _drawingText = _drawingText.substr(0, _drawingText.length()-1);
+                _bOmitted = true;
+            }
+        }
+        
+        if (_bAutoResizeFontSize) {
+            int size = 10;
+            while (!_bOmitted) {
+                _font.loadFont(_fontName, _fontSize++);
+                reconstructText();
+                if (_font.getStringBoundingBox(_drawingText,0,0).height + 20 >= _rect.height){
+                    _font.loadFont(_fontName, --_fontSize);
+                    reconstructText();
+                    _bOmitted = true;
                 }
             }
         }
@@ -80,9 +82,9 @@ public:
     void draw()
     {
         if (ofGetRectMode() == OF_RECTMODE_CENTER) {
-            _font->drawString(_drawingText, _rect.x-_rect.width/2., _rect.y-_rect.height/2.);
+            _font.drawString(_drawingText, _rect.x-_rect.width/2., _rect.y-_rect.height/2.);
         } else
-            _font->drawString(_drawingText, _rect.x, _rect.y);
+            _font.drawString(_drawingText, _rect.x, _rect.y);
         if (_bShowFrame) {
             ofPushStyle();
             ofNoFill();
@@ -91,12 +93,37 @@ public:
         }
     }
     
-       
+    void reconstructText(){
+        _drawingText = _text;
+        _drawingText.insert(0, L"\n");
+        ofRectangle r = _font.getStringBoundingBox(_text, _rect.x, _rect.y);
+        if (r.width > _rect.width || r.height > _rect.height) {
+            int i = 0;
+            wstring s_left = _text.substr(1);
+            while (_font.getStringBoundingBox(s_left, 0, 0).width > _rect.width) {
+                wstring tmp_s = L"";
+                int j = 0;
+                while (_font.getStringBoundingBox(tmp_s,0,0).width < _rect.width) {
+                    tmp_s = _drawingText.substr(i,j);
+                    j++;
+                }
+                i += j-1;
+                _drawingText.insert(i-1, L"\n");
+                s_left = _drawingText.substr(i);
+            }
+        }
+    }
+    
+    
 protected:
     wstring                     _drawingText;
     wstring                     _text;
+    string                      _fontName;
+    int                         _fontSize;
     ofRectangle                 _rect;
-    ofxTrueTypeFontUC*          _font;
+    ofxTrueTypeFontUC           _font;
     bool                        _bShowFrame;
+    bool                        _bAutoResizeFontSize;
+    bool                        _bOmitted;
 
 };
